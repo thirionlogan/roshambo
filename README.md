375e5502f6ed8a2c2b3c27a627dcbcc381cbc85c
Logan Thirion <thirionlogan@gmail.com>
2020-11-20 11:29:05 -0600

    allow complexity to be changed by user

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 setup by running `npm i` <br/>
 run a game by running `node index.js --move=rock` <br/>
-you can find valid moves by running: `node index.js --help`
+you can increase the complexity of the game by using `node index.js --move=rock --complexity=1` valid values are 0, 1, or 2<br/>
+you can find valid moves for the default complexity (0) by running: `node index.js --listrules`<br/>
+you can find valid moves for a certain level of complexity by running: `node index.js --listrules --complexity=1`<br/>
+these can all be combined like so: `node index.js --move=rock --complexity=2 --listrules`
--- index.js
+++ index.js
@@ -9,56 +9,71 @@ class Player {
     console.log(`${name} plays ${move}`);
   }
 
-  // static moveset = {
-  //   // move: [moves that beat move]
-  //   rock: ["paper"],
-  //   paper: ["scissors"],
-  //   scissors: ["rock"],
-  // };
+  static ruleSets = [
+    {
+      rock: ["paper"],
+      paper: ["scissors"],
+      scissors: ["rock"],
+    },
+    {
+      rock: ["paper", "spock"],
+      paper: ["scissors", "lizard"],
+      scissors: ["rock", "spock"],
+      lizard: ["scissors", "rock"],
+      spock: ["paper", "lizard"],
+    },
+    {
+      tree: ["lightning", "gun", "rock", "fire", "scissors", "snake", "human"],
+      wolf: ["gun", "rock", "fire", "scissors", "snake", "human", "tree"],
+      sponge: ["rock", "fire", "scissors", "snake", "human", "tree", "wolf"],
+      paper: ["fire", "scissors", "snake", "human", "tree", "wolf", "sponge"],
+      air: ["scissors", "snake", "human", "tree", "wolf", "sponge", "paper"],
+      water: ["snake", "human", "tree", "wolf", "sponge", "paper", "air"],
+      dragon: ["human", "tree", "wolf", "sponge", "paper", "air", "water"],
+      devil: ["tree", "wolf", "sponge", "paper", "air", "water", "dragon"],
+      lightning: ["wolf", "sponge", "paper", "air", "water", "dragon", "devil"],
+      gun: ["sponge", "paper", "air", "water", "dragon", "devil", "lightning"],
+      rock: ["paper", "air", "water", "dragon", "devil", "lightning", "gun"],
+      fire: ["air", "water", "dragon", "devil", "lightning", "gun", "rock"],
+      scissors: [
+        "water",
+        "dragon",
+        "devil",
+        "lightning",
+        "gun",
+        "rock",
+        "fire",
+      ],
+      snake: [
+        "dragon",
+        "devil",
+        "lightning",
+        "gun",
+        "rock",
+        "fire",
+        "scissors",
+      ],
+      human: ["devil", "lightning", "gun", "rock", "fire", "scissors", "snake"],
+    },
+  ];
 
-  // static moveSet = {
-  //   // move: [moves that beat move]
-  //   rock: ["paper", "spock"],
-  //   paper: ["scissors", "lizard"],
-  //   scissors: ["rock", "spock"],
-  //   lizard: ["scissors", "rock"],
-  //   spock: ["paper", "lizard"],
-  // };
-
-  static moveSet = {
-    tree: ["lightning", "gun", "rock", "fire", "scissors", "snake", "human"],
-    wolf: ["gun", "rock", "fire", "scissors", "snake", "human", "tree"],
-    sponge: ["rock", "fire", "scissors", "snake", "human", "tree", "wolf"],
-    paper: ["fire", "scissors", "snake", "human", "tree", "wolf", "sponge"],
-    air: ["scissors", "snake", "human", "tree", "wolf", "sponge", "paper"],
-    water: ["snake", "human", "tree", "wolf", "sponge", "paper", "air"],
-    dragon: ["human", "tree", "wolf", "sponge", "paper", "air", "water"],
-    devil: ["tree", "wolf", "sponge", "paper", "air", "water", "dragon"],
-    lightning: ["wolf", "sponge", "paper", "air", "water", "dragon", "devil"],
-    gun: ["sponge", "paper", "air", "water", "dragon", "devil", "lightning"],
-    rock: ["paper", "air", "water", "dragon", "devil", "lightning", "gun"],
-    fire: ["air", "water", "dragon", "devil", "lightning", "gun", "rock"],
-    scissors: ["water", "dragon", "devil", "lightning", "gun", "rock", "fire"],
-    snake: ["dragon", "devil", "lightning", "gun", "rock", "fire", "scissors"],
-    human: ["devil", "lightning", "gun", "rock", "fire", "scissors", "snake"],
-  };
-
-  static listRules() {
-    Object.entries(Player.moveSet).forEach((move) => {
+  static listRules(complexity = 0) {
+    Object.entries(Player.ruleSets[complexity]).forEach((move) => {
       console.log(`${move[0]} is beat by ${move[1].join(", ")}.`);
     });
   }
 
-  static compareMoves(player1, player2) {
+  static compareMoves(player1, player2, complexity = 0) {
     if (player1 instanceof Player || player2 instanceof Player) {
       throw new TypeError("Invalid Player!");
     }
+    const moveSet = Player.ruleSets[complexity];
 
     if (player1.move === player2.move) {
       console.log("~This round is a tie~");
-    } else if (Player.moveSet[player2.move].includes(player1.move)) {
+    } else if (moveSet[player2.move].includes(player1.move)) {
       console.log(`~${player1.name} wins.~`);
-    } else if (Player.moveSet[player1.move].includes(player2.move)) {
+    } else if (moveSet[player1.move].includes(player2.move)) {
       console.log(`~${player2.name} wins.~`);
     } else {
       throw new Error("Unexpected Error occured!");
@@ -71,25 +86,25 @@ class Player {
 }
 
 class HumanPlayer extends Player {
-  constructor() {
-    if (!(argv.move.toLowerCase() in Player.moveSet))
+  constructor(move, complexity = 0) {
+    if (!(move.toLowerCase() in Player.ruleSets[complexity]))
       throw new TypeError("Invalid move!");
-    super("Player", argv.move.toLowerCase());
+    super("Player", move.toLowerCase());
   }
 }
 
 class ComputerPlayer extends Player {
-  constructor() {
-    const moves = Object.keys(Player.moveSet);
+  constructor(complexity) {
+    const moves = Object.keys(Player.ruleSets[complexity]);
     super("Computer", moves[Math.floor(Math.random() * moves.length)]);
   }
 }
 
-if (argv.listRules || argv.listrules || argv.help || argv.Help) {
-  Player.listRules();
+if (argv.listRules || argv.listrules) {
+  Player.listRules(argv.complexity);
 }
 if (argv.move) {
-  const human = new HumanPlayer();
-  const computer = new ComputerPlayer();
-  Player.compareMoves(human, computer);
+  const human = new HumanPlayer(argv.move, argv.complexity);
+  const computer = new ComputerPlayer(argv.complexity);
+  Player.compareMoves(human, computer, argv.complexity);
 }
